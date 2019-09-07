2b571f4b65c5c3f666a3375da8baa60c65fdd510
Raja Karthikeyan <rkarthikeyan@icct.com>
2019-09-07 18:53:09 -0400

    Added default state value for Player X

--- exercise-kata/src/components/game/game.js
+++ exercise-kata/src/components/game/game.js
@@ -2,6 +2,19 @@ import React from 'react';
 import Board from '../Board/board'
 
 export default class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [
+        {
+          squares: Array(9).fill(null)
+        }
+      ],
+      stepNumber: 0,
+      xIsNext: true
+    };
+  }
+  
   render() {
     return (
       <div className="game">
