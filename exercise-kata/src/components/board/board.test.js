ab4a888183952d31d10b9b0a19db636b7007e6cd
Raja Karthikeyan <rkarthikeyan@icct.com>
2019-09-07 12:47:54 -0400

    added test to check 9 squares on board

--- exercise-kata/src/components/board/board.test.js
+++ exercise-kata/src/components/board/board.test.js
@@ -5,3 +5,8 @@ import {shallow, mount} from 'enzyme'
 it('renders without crashing', () => {
   shallow(<Board />);
 });
+
+it('contains nine <Square/> components', () => {
+  let wrapper = mount(<Board />);
+  expect(wrapper.find('.square')).toHaveLength(9);
+});
