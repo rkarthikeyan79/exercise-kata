5bc486adcd693adff2aa172c150d485c6cc0da60
Raja Karthikeyan <rkarthikeyan@icct.com>
2019-09-07 17:45:25 -0400

    implemented Render on Game comp

--- exercise-kata/src/components/game/game.js
+++ exercise-kata/src/components/game/game.js
@@ -2,4 +2,13 @@ import React from 'react';
 import Board from '../Board/board'
 
 export default class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board/>
+        </div>
+      </div>
+    );
+  }
 }
