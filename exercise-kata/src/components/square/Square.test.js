6ce5c6fc48fe6db2a526fbffcaa973499b460367
Raja Karthikeyan <rkarthikeyan@icct.com>
2019-09-07 06:57:14 -0400

    Adding test to check if it renders a value passed

--- exercise-kata/src/components/square/Square.test.js
+++ exercise-kata/src/components/square/Square.test.js
@@ -5,3 +5,8 @@ import { shallow } from 'enzyme'
 it('renders without crashing', () => {
   shallow(<Square/>);
 });
+
+it('renders the value passed to it', () => {
+    const value = 'X';
+    expect(shallow(<Square value={value}/>).text()).toBe(value);
+});
