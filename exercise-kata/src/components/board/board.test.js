c688c3061110ba67bdb46150e61be3d43935dd6e
Raja Karthikeyan <rkarthikeyan@icct.com>
2019-09-07 17:22:18 -0400

    Adding test for renderSquare method

--- exercise-kata/src/components/board/board.test.js
+++ exercise-kata/src/components/board/board.test.js
@@ -10,3 +10,9 @@ it('contains nine <Square/> components', () => {
   let wrapper = mount(<Board />);
   expect(wrapper.find('.square')).toHaveLength(9);
 });
+
+it('renders a square component when "renderSquare" is invoked', () => {
+  const square = shallow(<Board/>).instance().renderSquare();
+  expect(shallow(square).is('.Square')).toBe(false);
+});
+
