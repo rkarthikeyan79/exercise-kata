8bd3e6eeed037ad109cc0925f0498ff8ecb1c9c6
Raja Karthikeyan <rkarthikeyan@icct.com>
2019-09-08 17:47:41 -0400

    Applied some CSS style changes to look better

--- exercise-kata/src/App.js
+++ exercise-kata/src/App.js
@@ -7,9 +7,7 @@ class App extends Component {
   render() {
       return (
           <div className="App">
-              <h1>Tic-Tac-Toe!</h1>
               <Game/>
-              
           </div>
       );
   }
--- exercise-kata/src/components/game/game.js
+++ exercise-kata/src/components/game/game.js
@@ -113,13 +113,15 @@ export default class Game extends React.Component {
     return (
       <div className="game">
         <div className="game-board">
+        <div><h1>Tic-Tac-Toe!</h1></div>  
+        <div>{status}</div>
+        <br/>
           <Board
             squares={current.squares}
             onClick={i => this.handleClick(i)}
           />
         </div>
         <div className="game-info">
-          <div>{status}</div>
           <ol>{moves}</ol>
         </div>
       </div>
